1664c94e0dfb6cda58d4d8725bd34b574a01f727
sgooch <stephanie.gooch@kpmg.co.uk>
2020-07-05 17:41:17 +0100

    inital 12

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ data "archive_file" "workspace_stop_zip" {
 
 resource "aws_lambda_function" "workspace_start" {
   filename         = "${path.module}/output/workspace_start.zip"
-  function_name    = "workspace_start"
-  role             = "${aws_iam_role.iam_role_for_workspace_start_stop.arn}"
+  function_name    = "${var.function_prefix}workspace_start"
+  role             = aws_iam_role.iam_role_for_workspace_start_stop.arn
   handler          = "workspace_start.lambda_handler"
-  source_code_hash = "${data.archive_file.workspace_start_zip.output_base64sha256}"
+  source_code_hash = data.archive_file.workspace_start_zip.output_base64sha256
   runtime          = "python2.7"
   memory_size      = "512"
   timeout          = "150"
@@ -24,10 +24,10 @@ resource "aws_lambda_function" "workspace_start" {
 
 resource "aws_lambda_function" "workspace_stop" {
   filename         = "${path.module}/output/workspace_stop.zip"
-  function_name    = "workspace_stop"
-  role             = "${aws_iam_role.iam_role_for_workspace_start_stop.arn}"
+  function_name    = "${var.function_prefix}workspace_stop"
+  role             = aws_iam_role.iam_role_for_workspace_start_stop.arn
   handler          = "workspace_stop.lambda_handler"
-  source_code_hash = "${data.archive_file.workspace_stop_zip.output_base64sha256}"
+  source_code_hash = data.archive_file.workspace_stop_zip.output_base64sha256
   runtime          = "python2.7"
   memory_size      = "512"
   timeout          = "150"
@@ -36,21 +36,21 @@ resource "aws_lambda_function" "workspace_stop" {
 resource "aws_lambda_permission" "allow_cloudwatch_workspace_start" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.workspace_start.function_name}"
+  function_name = aws_lambda_function.workspace_start.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.workspace_start_cloudwatch_rule.arn}"
+  source_arn    = aws_cloudwatch_event_rule.workspace_start_cloudwatch_rule.arn
 
-  depends_on = ["aws_lambda_function.workspace_start"]
+  depends_on = [aws_lambda_function.workspace_start]
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_workspace_stop" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.workspace_stop.function_name}"
+  function_name = aws_lambda_function.workspace_stop.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.workspace_stop_cloudwatch_rule.arn}"
+  source_arn    = aws_cloudwatch_event_rule.workspace_stop_cloudwatch_rule.arn
 
-  depends_on = ["aws_lambda_function.workspace_stop"]
+  depends_on = [aws_lambda_function.workspace_stop]
 }
 
 resource "aws_cloudwatch_event_rule" "workspace_start_cloudwatch_rule" {
@@ -58,13 +58,13 @@ resource "aws_cloudwatch_event_rule" "workspace_start_cloudwatch_rule" {
 
   #to be run between 1-4am every 3 months starting from Feb on the first sunday of the month
 
-  schedule_expression = "${var.workspace_start_cron}"
+  schedule_expression = var.workspace_start_cron
 }
 
 resource "aws_cloudwatch_event_target" "workspace_start_lambda" {
-  rule      = "${aws_cloudwatch_event_rule.workspace_start_cloudwatch_rule.name}"
+  rule      = aws_cloudwatch_event_rule.workspace_start_cloudwatch_rule.name
   target_id = "lambda_target"
-  arn       = "${aws_lambda_function.workspace_start.arn}"
+  arn       = aws_lambda_function.workspace_start.arn
 }
 
 resource "aws_cloudwatch_event_rule" "workspace_stop_cloudwatch_rule" {
@@ -72,13 +72,13 @@ resource "aws_cloudwatch_event_rule" "workspace_stop_cloudwatch_rule" {
 
   #to be run between 1-4am every 3 months starting from Feb on the first sunday of the month
 
-  schedule_expression = "${var.workspace_stop_cron}"
+  schedule_expression = var.workspace_stop_cron
 }
 
 resource "aws_cloudwatch_event_target" "workspace_stop_lambda" {
-  rule      = "${aws_cloudwatch_event_rule.workspace_stop_cloudwatch_rule.name}"
+  rule      = aws_cloudwatch_event_rule.workspace_stop_cloudwatch_rule.name
   target_id = "lambda_target"
-  arn       = "${aws_lambda_function.workspace_stop.arn}"
+  arn       = aws_lambda_function.workspace_stop.arn
 }
 
 resource "aws_cloudwatch_log_group" "workspace_start_group" {
@@ -88,3 +88,4 @@ resource "aws_cloudwatch_log_group" "workspace_start_group" {
 resource "aws_cloudwatch_log_group" "workspace_stop_group" {
   name = "/aws/lambda/workspace_stop"
 }
+
