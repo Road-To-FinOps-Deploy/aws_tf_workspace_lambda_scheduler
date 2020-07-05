fe3e4baf3cd6fb24e9e417c97376561a1ee103f7
sgooch <stephanie.gooch@kpmg.co.uk>
2020-07-05 18:33:38 +0100

    added check for stop

--- lambda/workspace_start.py
+++ lambda/workspace_start.py
@@ -1,51 +1,41 @@
-#Importing the AWS SDK
+# Importing the AWS SDK
 import boto3
 
-#Creading workspace client object 
+# Creading workspace client object
 client = session.client("workspaces")
 
-#Description of all running workspace
+# Description of all running workspace
 response = client.describe_workspaces()
- 
+
+
 def lambda_handler(event, context):
 
-     #filter tag: <<start>> and <<stopped>> EC2 instances
-    filters = [{
-                'Name': 'tag:nightly', 
-                'Values': ['onoff']
-            },
-            {
-                'Name': 'instance-state-name', 
-                'Values': ['stopped']
-            }
-        ]
-        
-    #Looping over all workspaces in response
+    # filter tag: <<start>> and <<stopped>> EC2 instances
+    filters = [
+        {"Name": "tag:nightly", "Values": ["onoff"]},
+        {"Name": "instance-state-name", "Values": ["stopped"]},
+    ]
+
+    # Looping over all workspaces in response
     for workspace in response["Workspaces"]:
 
-        #Some temporary variables for each workspace
+        # Some temporary variables for each workspace
         state = str(workspace["State"])
         username = str(workspace["UserName"])
         workspaceId = str(workspace["WorkspaceId"])
         runningMode = workspace["WorkspaceProperties"]["RunningMode"]
-        
-        
-        #Starting turned off workspaces
-        if state=="STOPPED":
 
-            #Starting workspace with the id stored in varibale workspaceId
-            client.start_workspaces(StartWorkspaceRequests = [
-                {
-                    "WorkspaceId": workspaceId
-                }
-                ])
-        #Checking if the running mode is Auto Stop
-        if runningMode=="AUTO_STOP":
-            #Making the auto stop timeout 180 minutes
-            client.modify_workspace_properties(
-                WorkspaceId = workspaceId,
-                WorkspaceProperties = {
-                    'RunningModeAutoStopTimeoutInMinutes' : 180
-                }
+        # Starting turned off workspaces
+        if state == "STOPPED":
+
+            # Starting workspace with the id stored in varibale workspaceId
+            client.start_workspaces(
+                StartWorkspaceRequests=[{"WorkspaceId": workspaceId}]
             )
-            
+        # Checking if the running mode is Auto Stop
+        if runningMode == "AUTO_STOP":
+            # Making the auto stop timeout 180 minutes
+            client.modify_workspace_properties(
+                WorkspaceId=workspaceId,
+                WorkspaceProperties={"RunningModeAutoStopTimeoutInMinutes": 180},
+            )
